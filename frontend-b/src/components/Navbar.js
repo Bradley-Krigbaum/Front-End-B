1ca634c1a91eaeb9156af0f2a585d2887644cf21
Bradley-Krigbaum <bradley.krigbaum@gmail.com>
2020-02-26 21:00:13 -0700

    added rough navbar

---
+++ frontend-b/src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+    <div>
+        <section>
+            <div>
+                [ LOGO HERE ]
+            </div>
+            <section>
+                <div>
+                    [ SEARCH BAR ]
+                </div>
+                <div>
+                    [ LINK TO GUIDES ]
+                </div>
+                <div>
+                    [ FILTER ]
+                </div>
+            </section>
+            <div>
+                [ PROFILE PAGE ]
+            </div>
+        </section>
+    </div>
+    );
+}
+
+export default Navbar;
